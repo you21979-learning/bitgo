9911f54fa55403b1473508f04adf5daabca0ece3
you21979 <you2197901@gmail.com>
2016-11-07 22:07:01 +0900

    mod

--- ex1.js
+++ ex1.js
@@ -1,7 +1,33 @@
 var BitGoJS = require('bitgo');
+var Promise = require('bluebird');
 
-var BitgoAccessToken = function( token, env ){
+var bitgo_access_token = function( token, env ){
     return new BitGoJS.BitGo({ env: env || 'test', accessToken: token });
 }
 
-BitgoAccessToken(process.env['BITGO_TOKEN'])
+var bitgo_session = function( bitgo, opt ){
+    return new Promise(function(resolv, reject){
+        bitgo.session(opt || {}, function(err, res){
+            if(err) reject(err)
+            else resolv(res)
+        })
+    })
+}
+var bitgo_wallet_get = function( bitgo, id, type ){
+    return new Promise(function(resolv, reject){
+        bitgo.wallets().get({type:type || "bitcoin", id: id}, function(err, res){
+            if(err) reject(err)
+            else resolv(res)
+        })
+    })
+}
+
+var wallet_id = process.env['BITGO_WALLET_ID']
+var token = process.env['BITGO_TOKEN']
+
+var bitgo = bitgo_access_token(token)
+bitgo_session(bitgo).then(function(res){
+    return bitgo_wallet_get(bitgo, wallet_id).then(function(wallet){
+        console.log("Balance is: " + (wallet.balance() / 1e8).toFixed(4));
+    })
+})
