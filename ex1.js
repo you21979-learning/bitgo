a3b723e619f34c0caf5ce3c47438586288a008de
you21979 <you2197901@gmail.com>
2016-11-10 14:40:55 +0900

    mod

--- ex1.js
+++ ex1.js
@@ -1,33 +1,17 @@
-var BitGoJS = require('bitgo');
-var Promise = require('bluebird');
+const BitGoJS = require('bitgo');
 
-var bitgo_access_token = function( token, env ){
-    return new BitGoJS.BitGo({ env: env || 'test', accessToken: token });
-}
 
-var bitgo_session = function( bitgo, opt ){
-    return new Promise(function(resolv, reject){
-        bitgo.session(opt || {}, function(err, res){
-            if(err) reject(err)
-            else resolv(res)
-        })
+const wallet_id = process.env['BITGO_WALLET_ID']
+const token = process.env['BITGO_TOKEN']
+
+const bitgo_access_token = ( token, env ) => new BitGoJS.BitGo({ env: env || 'test', accessToken: token })
+
+const bitgo_get_wallet_balance = (bg, id) => 
+    bg.wallets().get({type:"bitcoin", id: id}).then(wallet => wallet.balance())
+
+const bg = bitgo_access_token(token)
+bg.session({}).then(res => 
+    bitgo_get_wallet_balance(bg, wallet_id).then(balance => {
+        console.log("Balance is: " + (balance / 1e8).toFixed(4));
     })
-}
-var bitgo_wallet_get = function( bitgo, id, type ){
-    return new Promise(function(resolv, reject){
-        bitgo.wallets().get({type:type || "bitcoin", id: id}, function(err, res){
-            if(err) reject(err)
-            else resolv(res)
-        })
-    })
-}
-
-var wallet_id = process.env['BITGO_WALLET_ID']
-var token = process.env['BITGO_TOKEN']
-
-var bitgo = bitgo_access_token(token)
-bitgo_session(bitgo).then(function(res){
-    return bitgo_wallet_get(bitgo, wallet_id).then(function(wallet){
-        console.log("Balance is: " + (wallet.balance() / 1e8).toFixed(4));
-    })
-})
+)
